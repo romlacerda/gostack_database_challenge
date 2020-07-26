909218527a16045f585de92fb692020bb038bf6e
Romulo Lacerda <rlcrweb@gmail.com>
2020-07-26 19:22:42 -0300

    feat: migrations

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,26 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column('varchar')
   title: string;
 
+  @Column('varchar')
   type: 'income' | 'outcome';
 
+  @Column('varchar')
   value: number;
 
+  @Column('varchar')
   category_id: string;
 
+  @Column('timestamp')
   created_at: Date;
 
+  @Column('timestamp')
   updated_at: Date;
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -3,9 +3,7 @@
 import Transaction from '../models/Transaction';
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
-  }
+  public async execute(): Promise<Transaction> {}
 }
 
 export default CreateTransactionService;
